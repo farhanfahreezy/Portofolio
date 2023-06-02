9e653d2857e275688a0dadcdd35906f25adcf36e
farhan <mohdfarhanfahrezy@gmail.com>
2023-06-02 23:44:22 +0700

    feat : completing project page

--- src/component/ProjectPreview.tsx
+++ src/component/ProjectPreview.tsx
@@ -1,4 +1,12 @@
-import { Box, Button, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Image,
+  Spacer,
+  Text,
+  VStack,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import { BsGithub } from "react-icons/bs";
 import { IoLogoGameControllerB } from "react-icons/io";
 
@@ -17,37 +25,56 @@ const ProjectPreview = ({
   repo,
   demo,
 }: ProjectPreviewProps) => {
+  const currentBreakpoint = useBreakpointValue({
+    xl: "xl",
+    md: "md",
+    sm: "sm",
+  });
   return (
     <VStack
-      w="300px"
+      maxW="400px"
+      h={currentBreakpoint !== "sm" ? "420px" : "380px"}
       justify="center"
       p={3}
-      bg="rgba(0,255,255,0.5)"
+      bg="rgba(19,19,19,0.8)"
       borderRadius={10}
+      justifyContent="flex-start"
       _hover={{ transform: "scale(1.02)" }}
       transition="transform 0.1s ease-in-out"
+      position="relative"
     >
       <Image src={image} borderRadius={10} />
       <Text fontWeight="bold" fontSize={24}>
         {title}
       </Text>
-      <Text>{subtitle}</Text>
-      <HStack>
+      <Text fontSize={currentBreakpoint !== "sm" ? 16 : 14}>{subtitle}</Text>
+      <HStack
+        justifyItems="space-between"
+        position="absolute"
+        bottom={3}
+        left={3}
+        right={3}
+      >
         <Button
           onClick={() => {
             window.open(repo);
           }}
+          _hover={{ transform: "scale(1.02)", bg: "yellow.500" }}
         >
           <BsGithub /> <Text marginLeft={2}> Source Code</Text>
         </Button>
+
         {demo && (
-          <Button
-            onClick={() => {
-              window.open(demo);
-            }}
-          >
-            <IoLogoGameControllerB /> <Text marginLeft={2}>Try</Text>
-          </Button>
+          <>
+            <Spacer />
+            <Button
+              onClick={() => {
+                window.open(demo);
+              }}
+            >
+              <IoLogoGameControllerB /> <Text marginLeft={2}>Try</Text>
+            </Button>
+          </>
         )}
       </HStack>
     </VStack>
--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,67 +1,88 @@
-import { Grid, GridItem, Stack, Text } from "@chakra-ui/react";
+import {
+  Grid,
+  GridItem,
+  Stack,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import { useState } from "react";
 import ProjectPreview from "../component/ProjectPreview";
 // import React from "react";
 
 const Project = () => {
   const [titleHover, setTitleHover] = useState(false);
+  const currentBreakpoint = useBreakpointValue({
+    xl: "xl",
+    lg: "lg",
+    sm: "sm",
+  });
   const projList = [
+    {
+      title: "farhanfahreezy | Portofolio",
+      subtitle:
+        "This is my personal portofolio. I create this web-app to display all of my skill, recent project, and also my latest CV.",
+      image: "project/porto.png",
+      repo: "https://github.com/farhanfahreezy/Portofolio",
+      demo: "https://farhanfahrezy.vercel.app/",
+    },
     {
       title: "15 Puzzle",
       subtitle:
-        "Classic 15 puzzle. The goal for this game is to arrange the tile from 1 to 15",
+        "Classic 15 puzzle. The goal for this game is to arrange the tile from 1 to 15. Can you beat this game?",
       image: "project/15puzzle.png",
       repo: "https://github.com/farhanfahreezy/15Puzzle",
       demo: "https://play15puzzle.vercel.app/",
     },
     {
-      title: "15 Puzzle",
+      title: "AkinatorGPT",
       subtitle:
-        "Classic 15 puzzle. The goal for this game is to arrange the tile from 1 to 15",
-      image: "project/15puzzle.png",
-      repo: "https://github.com/farhanfahreezy/15Puzzle",
+        "AkinatorGPT is a chatbot web-app that implements KMP and BM algorithms to provide users with answers to their questions.",
+      image: "project/akinator.png",
+      repo: "https://github.com/farhanfahreezy/Tubes3_13521058",
+      demo: "https://farhanfahreezy.github.io/Tubes3_13521058/",
     },
     {
-      title: "15 Puzzle",
+      title: "Store Organizer",
       subtitle:
-        "Classic 15 puzzle. The goal for this game is to arrange the tile from 1 to 15",
-      image: "project/15puzzle.png",
-      repo: "https://github.com/farhanfahreezy/15Puzzle",
+        "This desktop app is written in Java and it main functionality is to organize a store. You can add, remove, sell items and also giving rank to customer.",
+      image: "project/tubes2oop.png",
+      repo: "https://github.com/farhanfahreezy/KEN-Totalitas",
     },
     {
-      title: "15 Puzzle",
+      title: "Finding Home",
       subtitle:
-        "Classic 15 puzzle. The goal for this game is to arrange the tile from 1 to 15",
-      image: "project/15puzzle.png",
-      repo: "https://github.com/farhanfahreezy/15Puzzle",
+        "Finding Home is a quick problem solver for deciding path between 2 places. This web-app uses UCS and A* algorithm to find the route!",
+      image: "project/findinghome.png",
+      repo: "https://github.com/farhanfahreezy/Tucil3_UCS_Astar",
     },
     {
-      title: "15 Puzzle",
+      title: "Treasure Hunt Solver",
       subtitle:
-        "Classic 15 puzzle. The goal for this game is to arrange the tile from 1 to 15",
-      image: "project/15puzzle.png",
-      repo: "https://github.com/farhanfahreezy/15Puzzle",
+        "There is no safe place for a treasure. This desktop app designed to solve a maze with multiple treasure using BFS DFS algorithm.",
+      image: "project/treasurehunt.png",
+      repo: "https://github.com/farhanfahreezy/Tubes2_braveEagle69",
     },
     {
-      title: "15 Puzzle",
+      title: "Divide & Conquer",
       subtitle:
-        "Classic 15 puzzle. The goal for this game is to arrange the tile from 1 to 15",
-      image: "project/15puzzle.png",
-      repo: "https://github.com/farhanfahreezy/15Puzzle",
+        "This is a desktop app that i created using python with tkinter. This app generates random point and find the closest pair using DnC or BF.",
+      image: "project/dnc.png",
+      repo: "https://github.com/farhanfahreezy/Tucil2_13521092_13521106",
     },
     {
-      title: "15 Puzzle",
+      title: "Face Recognition",
       subtitle:
-        "Classic 15 puzzle. The goal for this game is to arrange the tile from 1 to 15",
-      image: "project/15puzzle.png",
-      repo: "https://github.com/farhanfahreezy/15Puzzle",
+        "Like it's name, this desktop app can recognize a face based on a certain dataset. This app uses eigenvalue to determine the person.",
+      image: "project/facerecognition.png",
+      repo: "https://github.com/farhanfahreezy/Algeo02-21041",
     },
     {
-      title: "15 Puzzle",
+      title: "Bukan Web Ngebajak",
       subtitle:
-        "Classic 15 puzzle. The goal for this game is to arrange the tile from 1 to 15",
-      image: "project/15puzzle.png",
-      repo: "https://github.com/farhanfahreezy/15Puzzle",
+        "This is my first web-app project using vanilla Js CSS HTML which displays a list of movies using the TMDB Rest API.",
+      image: "project/mvs.png",
+      repo: "https://github.com/farhanfahreezy/ProjectWebsiteGDSC-ITB",
+      demo: "https://farhanfahreezy.github.io/ProjectWebsiteGDSC-ITB/moviesearchengine.html",
     },
   ];
   return (
@@ -94,8 +115,21 @@ const Project = () => {
           Project
         </Text>
       </Text>
-      <Text fontSize={24}>Here are some of my recent work</Text>
-      <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+      <Text fontSize={24} mb={5}>
+        Here are some of my recent work
+      </Text>
+      <Grid
+        templateColumns={
+          currentBreakpoint === "xl"
+            ? "repeat(3, 1fr)"
+            : currentBreakpoint === "lg"
+            ? "repeat(2, 1fr)"
+            : "repeat(1,1fr)"
+        }
+        gap={6}
+        justifyItems="center"
+        mb="100px"
+      >
         {projList.map((proj) => (
           <GridItem>
             <ProjectPreview
