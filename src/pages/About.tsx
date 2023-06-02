f804a55dca69ce8d33e5c8323b02457e6476628f
farhan <mohdfarhanfahrezy@gmail.com>
2023-06-02 08:04:20 +0700

    fix : update responsive

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,35 +1,60 @@
-import { List, ListItem, ListIcon, Stack, Text } from "@chakra-ui/react";
+import {
+  List,
+  ListItem,
+  ListIcon,
+  Stack,
+  Text,
+  Button,
+  useBreakpointValue,
+  Show,
+  Box,
+} from "@chakra-ui/react";
 import PicSlideshow from "../component/PicSlideshow";
 import { AiFillFire } from "react-icons/ai";
 
 const About = () => {
+  const currentBreakpoint = useBreakpointValue({ base: "base", md: "md" });
   return (
     <Stack
       direction="column"
-      position="relative"
-      top="10%"
+      position="absolute"
       zIndex={1}
+      w="100vw"
       align="center"
-      w="100%"
       spacing="50px"
-      py="200px"
-      m={10}
+      paddingX={10}
+      top={currentBreakpoint === "md" ? "20vh" : "8vh"}
     >
-      <Stack direction="row" spacing={20} align="center">
+      <Stack
+        direction={currentBreakpoint === "md" ? "row" : "column"}
+        spacing={20}
+        align="center"
+        // mt={1000}
+      >
         <Stack direction="column" align="left">
           <Text fontSize={48} mb={5} fontWeight="bold">
             About Me
           </Text>
           <Text fontSize={24} maxW="800px">
-            Hi Everyone, my name is Mohammad Farhan Fahrezy from Pekanbaru,
-            Riau, Indonesia.
-          </Text>
-          <Text fontSize={24} maxW="800px" mb={5}>
-            I am currently pursuing my Bachelor Degree in Informatics
-            Engineering at Institut Teknologi Bandung.
+            Hi Everyone!👋 My name is{" "}
+            <Text as="span" color="yellow.500" fontWeight="bold">
+              Mohammad Farhan Fahrezy
+            </Text>
+            . I hail from the beautiful city of{" "}
+            <Text as="span" color="yellow.500" fontWeight="bold">
+              Pekanbaru
+            </Text>
+            , Riau, Indonesia.
           </Text>
           <Text fontSize={24} maxW="800px">
-            Apart from coding, some other activities that I love to do :
+            At the moment, I am passionately pursuing my Bachelor's Degree in{" "}
+            <Text as="span" color="yellow.500" fontWeight="bold">
+              Informatics Engineering
+            </Text>{" "}
+            at Institut Teknologi Bandung.
+          </Text>
+          <Text fontSize={24} maxW="800px">
+            Beyond the realm of coding, here's something i do in free time:
           </Text>
           <List fontSize={24} ml={10}>
             <ListItem>
@@ -51,9 +76,11 @@ const About = () => {
       <Text fontSize={48} fontWeight="bold">
         Professional Skillset
       </Text>
+      <Button>Updated Soon</Button>
       <Text fontSize={48} fontWeight="bold">
         Tools I Use
       </Text>
+      <Button>Updated Soon</Button>
     </Stack>
   );
 };
